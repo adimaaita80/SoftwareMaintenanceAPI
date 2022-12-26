9ce15c7b779f3bc52483ae7db4c915b7731564ab
Adi Maaita <adi.maaita80@gmail.com>
2022-12-26 23:42:11 +0300

    Created the EmployeeController

---
+++ SoftwareMaintenanceAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoftwareMaintenanceAPI.Models;
+
+namespace SoftwareMaintenanceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmpContext _context;
+
+        public EmployeeController(EmpContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet("GetAllEmployees")]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        [HttpPost("AddEmployee")]
+        public async Task<IActionResult> AddEmployee(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
