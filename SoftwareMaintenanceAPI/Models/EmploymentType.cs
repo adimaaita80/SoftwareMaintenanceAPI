755d2d0b33086061679ebc9b159ad55dbf1e5d06
adima <adima@DESKTOP-UPPDH0D>
2022-12-27 09:49:15 +0200

    Created the EmploymentType model.

---
+++ SoftwareMaintenanceAPI/Models/EmploymentType.cs
@@ -0,0 +1,8 @@
+﻿namespace SoftwareMaintenanceAPI.Models
+{
+    public class EmploymentType
+    {
+        public int Id { get; set; }
+        public string EmploymentTypeName { get; set; }
+    }
+}
